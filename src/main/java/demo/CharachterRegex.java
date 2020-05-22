c0971b4d71ce7d4b6752e8ac7ec7bcf7d8a80734
Dreams <Dreams@DESKTOP-IIN1NQL>
2020-05-22 15:15:29 +0530

    created regexEmail matcher for java

---
+++ src/main/java/demo/CharachterRegex.java
@@ -0,0 +1,25 @@
+package demo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CharachterRegex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final String regex = "[xyz]";
+		final String string = "cfyghjkgsuydssssssszzzzhhuhuhuhdrfgrrrrrXX";
+
+		final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		final Matcher matcher = pattern.matcher(string);
+
+		while (matcher.find()) {
+		    System.out.println("Full match: " + matcher.group(0));
+		    for (int i = 1; i <= matcher.groupCount(); i++) {
+		        System.out.println("Group " + i + ": " + matcher.group(i));
+		    }
+		}
+
+	}
+
+}
